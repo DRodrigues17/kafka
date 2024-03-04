c58ac921d85edbc89d8fc4038c615c3afdbed54f
Daniel Rodrigues <adanielluiz71@gmail.com>
2024-03-04 15:48:50 -0300

    projeto e docker-compose criados

---
+++ kafka-producer/src/test/java/com/drodrigues17/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drodrigues17.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
