1b315c73510dab98f7c96a8dea7731e0c8064eeb
Daniel Rodrigues <adanielluiz71@gmail.com>
2024-03-11 20:02:37 -0300

    json producer criado

---
+++ kafka-object-producer/src/test/java/com/drodrigues17/kafkaobjectproducer/KafkaObjectProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drodrigues17.kafkaobjectproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaObjectProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
